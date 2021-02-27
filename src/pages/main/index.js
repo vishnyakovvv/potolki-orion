887c517ae0cf33b989ed768fee88ff5a7ab659d5
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2021-02-27 19:38:21 +0400

    27f21

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,9 +2,9 @@
 
 // open tabs
 
-  (function tabs () {
-    let jsTriggers = document.querySelectorAll('.js-tab-trigger');
-  
+(function tabs () {
+   let jsTriggers = document.querySelectorAll('.js-tab-trigger');
+
     jsTriggers.forEach(function(trigger) {
         trigger.addEventListener('click', function () {
           let id = this.getAttribute('data-tab'),
@@ -40,142 +40,88 @@
 
   // window open
 
-//   (function windowOpen() {
-//     let callback = document.querySelector(".callback-modal_js"),
-//         buttonOpen = document.querySelector(".callback-open_js"),
-//         buttonClose = document.querySelector(".callback-close_js"),
-//         inputText = document.querySelector(".input-text-callback_js");
+  (function windowOpen() {
+    let callback = document.querySelector(".modal_js"),
+        buttonOpen = document.querySelectorAll(".js-open-modal"),
+        buttonClose = document.querySelector(".js-modal-close"),
+        inputText = document.querySelector(".input-text-callback_js");
 
+    buttonOpen.forEach(function(item) {
+      item.addEventListener("click", openCallbackPopup);
 
-//     buttonOpen.addEventListener("click", openCallbackPopup);
-
-//     function openCallbackPopup() {
-
-
-//         callback.classList.add("callback_open");
-//         inputText.focus();
-
-//         buttonClose.addEventListener("click", closeCallbackPopup);
-
-//         window.addEventListener("keydown", function (event) {
-//             if (event.code === "Escape") {
-//                 closeCallbackPopup()
-//             } 
-//         });
-
-//         callback.addEventListener("click", function (event) {
-//             if (event.target === callback) {
-//                 closeCallbackPopup()
-//             }
-//         });
-//     }
-
-//     function closeCallbackPopup() {
-//         callback.classList.remove("callback_open");
-//         buttonClose.removeEventListener("click", closeCallbackPopup);
-//     }
-// })();
-
-// document.addEventListener('DOMContentLoaded', function() {
-
-//   let modalButtons = document.querySelectorAll('.js-open-modal'),
-//       overlay = document.querySelector('#js-overlay-modal'),
-//       closeButtons = document.querySelectorAll('.js-modal-close');
-
-
-
-//       modalButtons.forEach(function(item){
-//         item.addEventListener('click', function(e) {
-
-//           e.preventDefault();
-
-//           let modalId = this.getAttribute('data-modal'),
-//               modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-
-
-
-//           modalElem.classList.add('active');
-//           overlay.classList.add('active');
-//         });
-
-//       });
-
-//       closeButtons.forEach(function(item){
-//         item.addEventListener('click', function(e) {
-//           let parentModal = this.closest('.callback__modal');
-
-//           parentModal.classList.remove('active');
-//           overlay.classList.remove('active');
-//         });
-//       });
-// })
-
-
-document.addEventListener('DOMContentLoaded', function() {
-
-  /* Записываем в переменные массив элементов-кнопок и подложку.
-     Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
-  var modalButtons = document.querySelectorAll('.js-open-modal'),
-      overlay      = document.querySelector('.js-overlay-modal'),
-      closeButtons = document.querySelectorAll('.js-modal-close');
-
-
-  /* Перебираем массив кнопок */
-  modalButtons.forEach(function(item){
-
-     /* Назначаем каждой кнопке обработчик клика */
-     item.addEventListener('click', function(e) {
-
-        /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-           люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-           Нужно подстраховаться. */
-        e.preventDefault();
-
-        /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-           и будем искать модальное окно с таким же атрибутом. */
-        var modalId = this.getAttribute('data-modal'),
-            modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-
-
-        /* После того как нашли нужное модальное окно, добавим классы
-           подложке и окну чтобы показать их. */
-        modalElem.classList.add('active');
-        overlay.classList.add('active');
-     }); // end click
-
-  }); // end foreach
-
-
-  closeButtons.forEach(function(item){
-
-     item.addEventListener('click', function(e) {
-        var parentModal = this.closest('.modal');
-
-        parentModal.classList.remove('active');
-        overlay.classList.remove('active');
-     });
-
-  }); // end foreach
-
-
-   document.body.addEventListener('keyup', function (e) {
-       var key = e.keyCode;
-
-       if (key == 27) {
-
-           document.querySelector('.modal.active').classList.remove('active');
-           document.querySelector('.overlay').classList.remove('active');
-       };
-   }, false);
-
-
-   overlay.addEventListener('click', function() {
-       document.querySelector('.modal.active').classList.remove('active');
-       this.classList.remove('active');
-   });
-
-
-
-
-}); // end ready
+      function openCallbackPopup() {
   
+  
+          callback.classList.add("callback_open");
+          inputText.focus();
+  
+          buttonClose.addEventListener("click", closeCallbackPopup);
+  
+          window.addEventListener("keydown", function (event) {
+              if (event.code === "Escape") {
+                  closeCallbackPopup()
+              } 
+          });
+  
+          callback.addEventListener("click", function (event) {
+              if (event.target === callback) {
+                  closeCallbackPopup()
+              }
+          });
+      }
+    });
+
+
+    function closeCallbackPopup() {
+        callback.classList.remove("callback_open");
+        buttonClose.removeEventListener("click", closeCallbackPopup);
+    }
+})();
+
+// top button
+
+(function () {
+  const buttonTop = document.querySelector(".btn-top__js");
+  if(!buttonTop) {
+      return;
+  }
+
+  function handleScroll() {
+      if (window.pageYOffset > 1000) {
+              buttonTop.classList.remove("visually-hidden");
+      } else {
+              buttonTop.classList.add("visually-hidden");
+      }
+  }
+  
+  function handleClick() {
+          window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+          });
+      }
+  
+  buttonTop.addEventListener("click", handleClick);
+  window.addEventListener("scroll", handleScroll);
+
+})();
+
+// mobile-menu
+
+(function() {
+    let mobileMenu = document.querySelector(".mobile-menu"),
+        buttonOpenMenu = document.querySelector(".button-open-menu_js"),
+        buttonCloseMenu = document.querySelector(".button-close-menu_js"),
+        mobileMenuFocus = document.querySelector(".mobile-menu-focus_js");
+
+        buttonOpenMenu.addEventListener("click", () => {
+            mobileMenu.classList.add("mobile-menu_open");
+            mobileMenuFocus.focus();
+        });
+
+        buttonCloseMenu.addEventListener("click", () => {
+            mobileMenu.classList.remove("mobile-menu_open");
+            mobileMenuFocus.focus();
+        });
+})();
+
